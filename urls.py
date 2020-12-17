cc2ac266b6961ce8a692bd2edb0aab564acce7bb
luizgodoy26 <luizgodoy26@gmail.com>
2020-12-17 20:02:28 -0300

    Adicionando app de produtos

--- urls.py
+++ urls.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from django.urls import path
 from .views import home, contacts, about, login_page, register_page
 
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 urlpatterns = [
     path('', home),
     path('contatos', contacts),
@@ -12,3 +16,6 @@ urlpatterns = [
 
     path('admin/', admin.site.urls),
 ]
+if settings.DEBUG:
+    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
